0c95ad75eff3539f0d8ec037d632fa8f645e853e
Dózsa Norbert <dozsa.n@gmail.com>
2017-05-14 17:10:36 +0200

    Allergia

--- app/views/medicalexam/complexAllergy.blade.php
+++ app/views/medicalexam/complexAllergy.blade.php
@@ -3,7 +3,65 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
+                Tudja mi az allergia ELSŐ számú oka, amit a legtöbb orvos nem vesz figyelembe?
+                Módszerünkkel szabadulhat meg az allergiás rohamoktól az állandó tüsszögéstől és a gyötrelmes orrdugulástól mindenféle vegyszer és gyógyszer nélkül!
+                A Komplex Allergia és Fül-Orr-Gégészeti Kivizsgálás pontosabb eredményekkel szolgál, mintha vérvizsgálatra, allergia tesztekre vagy ultrahangra, tükrözésre menne! Itt választ kap minden kérdésére a gyakran előforduló problémákkal és egyéb, ki nem mutatott betegségekkel kapcsolatban.
 
+                1
+                88-féle Allergén vizsgálata
+                Külön megnézzük mi okoz légúti tüneteket és mi okoz elváltozást a bőrön. (például: szőr, pollen, atka,parazita allergia, gombák által okozott allergia stb.)
+                2
+                Diagnózis szerint 32-féle légúti megbetegedés tesztelése.
+                Pl.: asztma, bronchitis, mandulagyulladás, stb.
+                3
+                Fül alapos vizsgálata
+                Külső és belső fül, a hallójáratok jobb és bal oldalról egyaránt, négyzetcentiméteres felbontásban tesztelésre kerül.
+                4
+                Kivizsgáljuk a felső-légutak, a szem és a fül nyirokrendszerét
+                5
+                Részletesen vizsgáljuk a tüdő és a hozzá kapcsolódó szervek állapotát
+                (légcsövet, hörgőket, bolyhokat, arcüreget, garatot, homloküreget stb.)
+                6
+                Ételteszt
+                Nemcsak az alapanyagokat vizsgáljuk, hanem elkészítési mód szerint is megtudja, mit ehet és hogyan. (húsfélék, halfélék, zöldségfélék, gyümölcsök, magvak, gabonafélék, olajok, mézek, alkohol tartalmú italok, kávéfélék, teafélék, tojásfélék, tejtermékek, gombák) Összesen 207 élelmiszert vizsgálunk.
+                7
+                Hozott anyag vizsgálata
+                Olyan anyagok, amit rendszeres jelleggel használ . (Pl: gyógyszerek, táplálékkiegészítők, vitaminok) Kiderül, hogy hogyan reagál ezekre az Ön szervezete, milyen hatással van rá. Tehát, ha van olyan tisztálkodó szer, vitamin, bármi, ami kapcsolatba kerül az Ön szervezetével és nem tudja, hogy káros-e vagy sem, akkor bevizsgáljuk.
+                8
+                Komplex állapotfelmérés:
+                A szervek közötti csapatmunkát vizsgáljuk. Kiderül, hol van alulműködés vagy túlműködés az egyes szerveinél.
+                9
+                Azonnali vérkép meghatározása:
+                Szúrás nélkül, az akupunktúrás pontokon keresztül: 20 paraméter vizsgálata pl. máj-, vesefunkciók, vércukorszint, koleszterinszint stb.
+                10
+                Parazita, gomba és baktérium teszt
+                Élősködőket, parazitákat és gombákat vizsgálunk. Megnézzük bármelyikből van-e betegségeket okozó mennyiség a szervezetében.56-féle parazita, 48-féle baktérium és 38-féle gomba jelenlétének tesztelése
+                11
+                Vegyszerterheltség vizsgálata
+                Itt kiderül, van-e valamilyen gyógyszermaradvány, radioaktív anyag, nehézfém a szervezetében.
+                12
+                Vírusok tesztelése
+                11 vírust vizsgálunk meg, hogy előfordul-e az Ön szervezetében betegséget okozó mennyiségben. (Köztük a DNS-t károsító vírust is.)
+                13
+                Vitamin és nyomelem háztartás tesztelése
+                Kiderül, hogy a szervezete működéséhez minden szükséges vitamin és nyomelem megvan-e.
+                14
+                Homeopátia-teszt 1.:
+                Homeopátiás készítmények, táplálékkiegészítők szükségességének vizsgálata.
+                15
+                Homeopátia-teszt 2.:
+                A kezelési programok szükségességének tesztelése
+                +
+                Azonnali eredmény kiértékelés, személyes konzultációval
+                Így nem kell napokat, heteket várnia a vizsgálatok eredményeire, azonnal elkezdheti az Önnek kialakított terápiát és azonnal a gyógyulás útjára léphet, egészségesebb, sikeresebb emberként élheti életét.
+                +
+                Személyre szabott terápia
+                Összeállítása minden rendelkezésre álló eszközünk felhasználásával
+                +
+                Életviteli, étkezési tanácsadás
+                Komplex Allergia és Fül-Orr-Gégészeti Kivizsgálás ára:25.900 Ft
+                Hogyan zajlik a kivizsgálás?
+                Ön megérkezik a megbeszélt időpontra. Diagnosztánk fogadja Önt, részletesen megbeszélik, hogy Ön milyen céllal érkezett rendelőnkbe. Ezután a kéz és lábfején található akupunktúrás végpontokon keresztül végzi szakemberünk a vizsgálatokat, két elektróda érintésének segítségével. Semmilyen fájdalmas vagy kellemetlen beavatkozás nem történik. Miután szakemberünk minden szükséges tesztet elvégzett, kiértékeli az eredményeket, elmagyaráz mindent Önnek, majd kezelési javaslatot tesz. A folytatás sikeressége ezt követően már csak Önön múlik. A terápia alatt szakértőnk végig követi az Ön gyógyulását, szükség esetén ellátja Önt további tanácsokkal a még hatékonybb terápia érdekében. És a gyógyulás után sem hagyjuk Önt magára, folyamatos életviteli tanácsokkal, konzultációkkal, kontroll-vizsgálatokkal segítjünk Önt, hogy megőrizze az elért állapotát.
             </div>
         </div>
     </div>
