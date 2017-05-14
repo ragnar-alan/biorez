0b74150209536885827d78a08d73fb2ca38aa0db
Boros Tamás <boros.tamas456@gmail.com>
2017-05-14 16:19:54 +0200

    medical exams in controller and routes

--- app/controllers/MedicalExaminationController.php
+++ app/controllers/MedicalExaminationController.php
@@ -10,4 +10,32 @@ class MedicalExaminationController extends BaseController {
     public function index() {
         return View::make("medicalexam.index");
     }
+
+    public function standardExamination() {
+        return View::make("medicalexam.standardExam");
+    }
+
+    public function complexManExamination() {
+        return View::make("medicalexam.complexMan");
+    }
+
+    public function complexWomanExamination() {
+        return View::make("medicalexam.complexWoman");
+    }
+
+    public function complexOncoExamination() {
+        return View::make("medicalexam.complexOnco");
+    }
+
+    public function complexRheumaExamination() {
+        return View::make("medicalexam.complexRheuma");
+    }
+
+    public function pregnantExamination() {
+        return View::make("medicalexam.pregnantExam");
+    }
+
+    public function complexAllergyExamination() {
+        return View::make("medicalexam.complexAllergy");
+    }
 }
