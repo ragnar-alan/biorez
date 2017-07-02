cc36a6ca45d3e954f01c7589c6fdad31523daa00
Boros Tamás <boros.tamas456@gmail.com>
2017-07-03 00:16:56 +0200

    email template

---
+++ app/views/emails/info/infoemail.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<h2>Érdeklődő</h2>
+
+<div>
+    Az érdeklődő neve: {{ $name }}<br>
+    Az érdeklődő email címe: {{ $userEmail }}
+    Az érdeklődő üzenete: {{ $message }}
+
+</div>
+</body>
+</html>
