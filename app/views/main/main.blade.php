60ad66104f02c3c77c4beaccd27988fe950cf6d3
Boros Tamás <boros.tamas456@gmail.com>
2018-07-21 20:30:04 +0200

    changes in text

--- app/views/main/main.blade.php
+++ app/views/main/main.blade.php
@@ -13,24 +13,17 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1>
-                    Biorezonanciás állapotfelmérés!
+                    Biorezonanciás állapotfelmérés - személyre szabott tanácsadás - kezelés!
                 </h1>
-                <p>
-                    A módszer az egész testet vizsgálja, az akupunktúrás végpontokon keresztül, szúrás, vágás, kellemetlenség nélkül. Az egészséges állapottól való legkisebb eltérés is mérhető, így már akár az elváltozások megjelenése előtt jelezhetők egyes betegségek! A kezelés lényege, hogy az egészséges rezgéseket megerősítse, a hibás, kóros rezgéseket pedig megszüntesse. A vizsgálat során felállított állapotfelmérés eredménye után azonnal megkezdhető a személyre szabott kezelés, melynek során egészséges sejtekre jellemző rezgésekkel segítjük a szervezet harmonikus működését. A Biorezonanciás vizsgálat és energetikai kezelés nem helyettesíti, és nem válthatja ki a szükséges orvosi diagnosztikát és annak módszereit, illetve a szükséges orvos-gyógyászati terápiát - kezelést!
+                <p style="font-size: 20px;line-height: 30px;">
+                    A módszer az egész testet vizsgálja, az akupunktúrás végpontokon keresztül, szúrás, vágás, kellemetlenség nélkül. Az egészséges állapottól való legkisebb eltérés is mérhető, így már akár az elváltozások megjelenése előtt jelezhetők egyes betegségek! A kezelés lényege, hogy az egészséges rezgéseket megerősítse, a hibás, kóros rezgéseket pedig megszüntesse.
                 </p>
+                <p style="font-size: 20px;line-height: 30px;">A vizsgálat során felállított állapotfelmérés eredménye után azonnal megkezdhető a személyre szabott kezelés, melynek során egészséges sejtekre jellemző rezgésekkel segítjük a szervezet harmonikus működését. A Biorezonanciás vizsgálat és energetikai kezelés nem helyettesíti, és nem válthatja ki a szükséges orvosi diagnosztikát és annak módszereit, illetve a szükséges orvos-gyógyászati terápiát - kezelést!</p>
                 <div class="row">
                     <div class="col-lg-12">&nbsp;</div>
                 </div>
                 <div class="row">
-                    <div class="col-lg-4 text-center main-box">
-                        <i class="circle fa fa-history fa-5x"></i>
-                        <h3>Bio rezonancia történelem</h3>
-                            <p class="text-justify">A szervezet egészének informatikai rehabilitációjával foglalkozó terápiás irányzatot összefoglaló néven biorezonancia terápiának (BRT) nevezik. A biorezonancia terápiát az űrhajósok egészségmegőrzésére kezdték kifejleszteni.
-                                Az 1920-as évektől foglalkoznak tudósok a bioinformatikával. A szervezet működésének (az életnek) alapja a jól működő, összehangolt sejtinformációs rendszer.
-                            </p>
-                        <a href="{{url('history')}}" class="btn btn-success">Tovább</a>
-                    </div>
-                    <div class="col-lg-4 text-center main-box">
+                    <div class="col-lg-6 text-center main-box">
                         <i class="circle fa fa-question-circle fa-5x"></i>
                         <h3>Mikor alkalmazható?</h3>
                         <ul class="text-justify originalHtml">
@@ -44,12 +37,13 @@
                             <li>Hajhullás, kopaszodás esetén.</li>
                         </ul>
                     </div>
-                    <div class="col-lg-4 text-center main-box">
+                    <div class="col-lg-6 text-center main-box">
                         <i class="circle fa fa-user-md fa-5x"></i>
                         <h3>Ismerj meg</h3>
                         <p class="text-justify">Daru Bálint vagyok, 1978-óta dolgozom az egészségügyben, jelenleg a Bács-Kiskun Megyei Kórházban.
                             Hamar megtapasztaltam, hogy a nyugati orvoslás mellett másra is szükség van a helyes és teljes értékű betegellátáshoz.
                             Kerestem azt a lehetőséget, módszert, amivel az eddig tanultakat kiegészíthettem.</p>
+                        <img src="http://biorez.eu/assets/image/myself.jpg" alt="" class="img-responsive" style="width: 300px;">
                         <a href="{{url('about')}}" class="btn btn-success">Tovább</a>
                     </div>
                 </div>
--- app/views/medicalexam/index.blade.php
+++ app/views/medicalexam/index.blade.php
@@ -9,7 +9,7 @@
         <div class="row">
             <div class="col-lg-4 text-center main-box">
                 <i class="circle circle-icons fa fa-user-o fa-5x"></i>
-                <h3>Standard vizsgálat</h3>
+                <h3>Alap vizsgálat</h3>
                 <p class="text-justify indentation">
                     Ez a vizsgálat a kéz és láb akupunktúrás pontjain keresztül történik, ami a szervezet működéséről és élettani helyzetéről 80-90%- os képet ad.
                     <ul class="text-left originalHtml" style="padding-left:15px;">
@@ -21,7 +21,7 @@
             </div>
             <div class="col-lg-4 text-center main-box">
                 <i class="circle circle-icons fa fa-medkit fa-5x"></i>
-                <h3>Komplex allergia vizsgálat</h3>
+                <h3>Komplex vizsgálat</h3>
                 <p class="text-justify indentation">
                     Tudja mi az allergia ELSŐ számú oka, amit a legtöbb orvos nem vesz figyelembe? Módszerünkkel szabadulhat meg az allergiás rohamoktól az állandó tüsszögéstől és a gyötrelmes orrdugulástól mindenféle vegyszer és gyógyszer nélkül! A Komplex Allergia és Fül-Orr-Gégészeti Kivizsgálás pontosabb eredményekkel szolgál, mintha vérvizsgálatra, allergia tesztekre vagy ultrahangra, tükrözésre menne!
                 </p>
@@ -29,48 +29,36 @@
             </div>
             <div class="col-lg-4 text-center main-box">
                 <i class="circle circle-icons fa fa-user-md fa-5x"></i>
-                <h3>Komplex onkológiai vizsgálat</h3>
+                <h3>Ellenőrző vizsgálat</h3>
                 <p class="text-justify indentation">A Komplex Onkológiai Kivizsgálás pontosabb eredményekkel szolgál, mintha CT-re, MR-re, vérvizsgálatra vagy ultrahangra, tükrözésre menne! Itt választ kap minden kérdéseire a ki nem mutatott betegségekkel kapcsolatban is
                 </p>
                 <a href="{{action('MedicalExaminationController@complexOncoExamination')}}" class="btn btn-success">Tovább</a>
             </div>
-            {{--<div class="col-lg-4 text-center main-box">--}}
-                {{--<i class="circle circle-icons fa fa-male fa-5x"></i>--}}
-                {{--<h3>Komplex férfi vizsgálat</h3>--}}
-                {{--<p class="text-justify indentation">--}}
-                    {{--A Komplex Férfi Kivizsgálás pontosabb eredményekkel szolgál, mintha CT-re, MR-re, vérvizsgálatra vagy ultrahangra, tükrözésre menne! Itt választ kap minden kérdésére a férfiaknál gyakran előforduló problémákkal és egyéb, ki nem mutatott betegségekkel kapcsolatban.--}}
-                {{--</p>--}}
-                {{--<a href="{{action('MedicalExaminationController@complexManExamination')}}" class="btn btn-success">Tovább</a>--}}
-            {{--</div>--}}
-            {{--<div class="col-lg-4 text-center main-box">--}}
-                {{--<i class="circle circle-icons fa fa-female fa-5x"></i>--}}
-                {{--<h3>Komplex női vizsgálat</h3>--}}
-                {{--<p class="text-justify indentation">--}}
-                    {{--A Komplex Női Kivizsgálás részletesebb, nagyságrendekkel több információval szolgál, mintha klasszikus nőgyógyászati szűrésre, vérvizsgálatra vagy ultrahangra menne! Itt választ kap minden kérdésére a nőknél gyakran előforduló problémákkal, a női test működésének különleges összefüggéseivel, és más, ki nem mutatott betegségekkel kapcsolatban.--}}
-                {{--</p>--}}
-                {{--<a href="{{action('MedicalExaminationController@complexWomanExamination')}}" class="btn btn-success">Tovább</a>--}}
-            {{--</div>--}}
         </div>
         <div class="row">
-
-            {{--<div class="col-lg-4 text-center main-box">--}}
-                {{--<i class="circle circle-icons fa fa-medkit fa-5x"></i>--}}
-                {{--<h3>Komplex rheuma vizsgálat</h3>--}}
-                {{--<p class="text-justify indentation">--}}
-                    {{--A Komplex Reumatológiai Kivizsgálás sokkal több információt ad testéről, mint a megszokott klinikai vizsgálatok! Itt választ kap minden kérdésére reumatológiai problémáival és egyéb, ki nem mutatott betegségekkel kapcsolatban.--}}
-                {{--</p>--}}
-                {{--<a href="{{action('MedicalExaminationController@complexRheumaExamination')}}" class="btn btn-success">Tovább</a>--}}
-            {{--</div>--}}
+            <div class="col-lg-12">
+                <h2>Kezelés</h2>
+                <h3>2.000 Ft / kezelés</h3>
+            </div>
         </div>
-        {{--<div class="row">--}}
-            {{--<div class="col-lg-offset-4 col-lg-4 text-center main-box">--}}
-                {{--<i class="circle circle-icons fa fa-child fa-5x"></i>--}}
-                {{--<h3>Baba váró vizsgálat</h3>--}}
-                {{--<p class="text-justify">--}}
-                    {{--Akkor Babaváró vizsgálatunkkal elképzelhető, hogy élete során most került a legközelebb édes kisbabája megszületéséhez…--}}
-                {{--</p>--}}
-                {{--<a href="{{action('MedicalExaminationController@pregnantExamination')}}" class="btn btn-success">Tovább</a>--}}
-            {{--</div>--}}
-        {{--</div>--}}
+        <div class="row">
+            <div class="col-lg-12">
+                <p style="font-size: 20px;line-height: 30px;">
+                    A kezelés lényege, hogy az egészséges rezgéseket megerősítse, a hibás, kóros rezgéseket pedig megszüntesse.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>Táplálék kiegészítő</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <p style="font-size: 20px;line-height: 30px;">
+                    A kezelés lényege, hogy az egészséges rezgéseket megerősítse, a hibás, kóros rezgéseket pedig megszüntesse.
+                </p>
+            </div>
+        </div>
     </div>
 @stop
